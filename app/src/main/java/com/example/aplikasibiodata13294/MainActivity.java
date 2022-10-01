db8b429c92d20cb6a87bf8eee45ae7ec3c2758c6
AdeHidayat <adeeehidayat63@gmail.com>
2022-10-01 15:35:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/aplikasibiodata13294/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.aplikasibiodata13294;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void showMap (View view){
+        Uri uri = Uri.parse("geo:-6.970419,110.252339=");
+        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(it);
+    }
+    public void telepon (View view){
+        Uri uri = Uri.parse("tel:089644135314");
+        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(it);
+    }
+    public void email (View view){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"111202113294@mhs.dinus.ac.id"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Email dari Aplikasi Android");
+
+        try {
+            startActivity(Intent.createChooser(intent, "Ingin Mengirim Email ?"));
+        } catch (android.content.ActivityNotFoundException ex) {
+
+        }
+    }
+}
